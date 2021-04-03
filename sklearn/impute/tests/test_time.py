127fa029835306a708db3f2c4ced7c0f42ba5d95
SeanApplebaum <sean.applebaum@mail.utoronto.ca>
2021-04-03 14:44:31 -0400

    Cleaned and polished time test cases

--- sklearn/impute/tests/test_time.py
+++ sklearn/impute/tests/test_time.py
@@ -9,10 +9,14 @@ import pytest
 # Total Original Time:  97.91s
 # Total new Time:       48.07s
 
-def gen_matrix(x, y, p=0.1):
+perc = [0,0.1,0.5,0.9,1]
+small_n = [100,300,500]
+large_n = [1000,3000,5000]
+
+def gen_matrix(x, y, p):
     np.random.seed(1)
     X = np.random.random([x, y])
-    X = pd.DataFrame(X).mask(X < p)
+    X = pd.DataFrame(X).mask(X <= p)
     return X
 
 
@@ -33,8 +37,8 @@ def relative_assert(new, old):
 def output_res(old_end,old_start,end,start):
     print("\nOld time:", round((old_end-old_start),4) ,", New time:", round((end-start),4),",", str(round(((old_end-old_start)/(end-start) - 1)*100, 2))+"% improved")
 
-@pytest.mark.parametrize("n,p",[(n, p) for p in [0,0.1,0.5,0.9,1] for n in [100,200,300,400,500]])
-def test_1_by_n(n,p):
+@pytest.mark.parametrize("n,p",[(n, p) for p in perc for n in small_n])
+def test_time_1_by_n(n,p):
     X = gen_matrix(1, n, p)
 
     start, end = run_test(X)
@@ -44,8 +48,8 @@ def test_1_by_n(n,p):
 
     relative_assert(end-start, old_end-old_start)
 
-@pytest.mark.parametrize("N,p",[(N, p) for p in [0,0.1,0.5,0.9,1] for N in [1000,2000,3000,4000,5000,10000]])
-def test_1_by_N(N,p):
+@pytest.mark.parametrize("N,p",[(N, p) for p in perc for N in large_n])
+def test_time_1_by_N(N,p):
     X = gen_matrix(1, N, p)
 
     start, end = run_test(X)
@@ -55,9 +59,9 @@ def test_1_by_N(N,p):
 
     relative_assert(end-start, old_end-old_start)
 
-@pytest.mark.parametrize("n",[100,200,300,400,500])
-def test_n_by_1(n):
-    X = gen_matrix(n, 1)
+@pytest.mark.parametrize("n,p",[(n, p) for p in perc for n in small_n])
+def test_time_n_by_1(n,p):
+    X = gen_matrix(n, 1, p)
 
     start, end = run_test(X)
     old_start, old_end = run_test(X, old=True)
@@ -66,9 +70,9 @@ def test_n_by_1(n):
 
     relative_assert(end-start, old_end-old_start)
 
-@pytest.mark.parametrize("N",[1000,2000,3000,4000,5000,10000])
-def test_N_by_1(N):
-    X = gen_matrix(N, 1)
+@pytest.mark.parametrize("N,p",[(N, p) for p in perc for N in large_n])
+def test_time_N_by_1(N,p):
+    X = gen_matrix(N, 1, p)
 
     start, end = run_test(X)
     old_start, old_end = run_test(X, old=True)
@@ -77,8 +81,8 @@ def test_N_by_1(N):
 
     relative_assert(end-start, old_end-old_start)
 
-@pytest.mark.parametrize("n1,n2,p",[(n1,n2,p) for p in [0,0.1,0.5,0.9,1] for n1 in [100,200,300,400,500] for n2 in [100,200,300,400,500]])
-def test_n_by_n(n1,n2,p):
+@pytest.mark.parametrize("n1,n2,p",[(n1,n2,p) for p in perc for n1 in small_n for n2 in small_n])
+def test_time_n_by_n(n1,n2,p):
     X = gen_matrix(n1, n2, p)
 
     start, end = run_test(X)
@@ -88,34 +92,35 @@ def test_n_by_n(n1,n2,p):
 
     relative_assert(end-start, old_end-old_start)
 
-# @pytest.mark.parametrize("n,N",[(n,N) for n in [100,200,300,400,500] for N in [1000,2000,3000,4000,5000]])
-# def test_n_by_N(n,N):
-#     X = gen_matrix(n, N)
+@pytest.mark.parametrize("n,N,p",[(n,N,p) for p in perc for n in small_n for N in large_n])
+def test_time_n_by_N(n,N,p):
+    X = gen_matrix(n, N, p)
+
+    start, end = run_test(X)
+    old_start, old_end = run_test(X, old=True)
+
+    output_res(old_end,old_start,end,start)
+
+    relative_assert(end-start, old_end-old_start)
+
+@pytest.mark.parametrize("N,n,p",[(N,n,p) for p in perc for n in small_n for N in large_n])
+def test_time_N_by_n(N,n,p):
+    X = gen_matrix(N, n, p)
+
+    start, end = run_test(X)
+    old_start, old_end = run_test(X, old=True)
+
+    output_res(old_end,old_start,end,start)
+
+    relative_assert(end-start, old_end-old_start)
+
+# @pytest.mark.parametrize("N1,N2,p",[(N1,N2,p) for p in perc for n in large_n for N in large_n])
+# def test_time_N_by_N(N1, N2):
+#     X = gen_matrix(N1, N2, p)
 
 #     start, end = run_test(X)
 #     old_start, old_end = run_test(X, old=True)
 
 #     output_res(old_end,old_start,end,start)
 
-#     assert (end-start) < (old_end-old_start)
-
-# @pytest.mark.parametrize("N,n",[(N,n) for n in [100,200,300,400,500] for N in [1000,2000,3000,4000,5000]])
-# def test_N_by_n(N,n):
-#     X = gen_matrix(N, n)
-
-#     start, end = run_test(X)
-#     old_start, old_end = run_test(X, old=True)
-
-#     output_res(old_end,old_start,end,start)
-
-#     assert (end-start) < (old_end-old_start)
-
-# def test_N_by_n():
-#     X = gen_matrix(10000, 100)
-
-#     start, end = run_test(X)
-#     old_start, old_end = run_test(X, old=True)
-
-#     output_res(old_end,old_start,end,start)
-
-#     assert (end-start) < (old_end-old_start)
+#     relative_assert(end-start, old_end-old_start)
